e48a209856618945abbfda2156959eda836379f2
amrukwa <a.mrukwa00@gmail.com>
2020-10-19 13:18:57 +0200

    Add sln

--- Project/dijikstra/dijikstra/Source.cpp
+++ Project/dijikstra/dijikstra/Source.cpp
@@ -1,7 +1,14 @@
 # include <iostream>
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
 
 
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+	int* x = (int*)malloc(3 * sizeof(int));
+	free(x);
 	return 0;
 }
