da15dffe9a8a1fd6712ec25e5534ea1181fd2fc7
amrukwa <a.mrukwa00@gmail.com>
2020-10-26 16:23:45 +0100

    Add directory from the console

--- Project/dijikstra/dijikstra/Source.cpp
+++ Project/dijikstra/dijikstra/Source.cpp
@@ -3,11 +3,35 @@
 #include <crtdbg.h>
 # include "Graph.h"
 
+std::ifstream ask_for_directory()
+{
+	std::ifstream ifile;
+	std::cout << "Please specify the directory of the file with the cities." << std::endl;
+	std::string dir;
+	std::cin >> dir;
+	try {
+		ifile.open(dir);
+		if (!ifile)
+		{
+			throw 1;
+		}
+	}
+	catch(int i)
+	{
+		std::cout << "No such file." << std::endl;
+		ifile = ask_for_directory();
+	}
+	return ifile;
+}
+
 
 int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
-	Graph graph;
+	std::ifstream datafile = ask_for_directory();
+	Graph graph(datafile);
+	datafile.close();
+	system("pause");
 	return 0;
 }
